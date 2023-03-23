f4883d606cbe706d4f9f6680331f71b5da5c261a
Shaharyar <shaharyarahmed02@gmail.com>
2023-03-24 01:29:18 +0500

    pep website

---
+++ app.js
@@ -0,0 +1,56 @@
+let input = document.querySelector('.input').value
+// console.log(input)
+let c = "4"; // String
+// console.log(typeof(c))
+let d = 4; // Number
+// console.log(typeof(d))
+
+
+
+// if (c === d) {
+//     console.log("Sucess")
+// }
+// else if(c == d){
+//     console.log("Else if true")
+
+// }
+// else{
+//     console.log("False")
+
+// }
+
+
+
+
+
+// if (c === d) {
+//     if (x === y) {
+//         g = h;
+//     }
+//     else if (a === b) {
+//         g = h;
+//     }
+//     else {
+//         e = f;
+//     }
+// }
+// else {
+//     e = f;
+// }
+
+
+
+var city0 = "Atlanta";
+var city1 = "Baltimore";
+var city2 = "Chicago";
+var city3 = "Denver";
+var city4 = "Los Angeles";
+var city5 = "Seattle";
+
+                   // 0        // 1  //2  //3
+let countries = ["Pakistan", "India", 4, false]
+console.log(countries[2]);
+
+
+
+
